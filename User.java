d7f921da4a25eb4aa24d232daf9ad01f2c9df670
dipesh2901 <67797922+dipesh2901@users.noreply.github.com>
2021-08-13 23:04:41 +0530

    Program32

---
+++ User.java
@@ -0,0 +1,63 @@
+package com.neosoft.Task;
+
+public class User {
+	private int uid;
+	private String name, email, mobile, username, password;
+
+	public User(int uid, String email, String mobile, String username, String password) {
+		this.uid = uid;
+		this.email = email;
+		this.mobile = mobile;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
